942a25cff4fcfbd1d76b36bc2cb05e1a49250092
Natthasit Thitithammakun <6610685155@student.tu.ac.th>
2025-09-11 05:42:36 +0700

    Edit Project setting and urls

--- Room_reserve/urls.py
+++ Room_reserve/urls.py
@@ -20,6 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("users/", include("users.urls")),
-    path("", include("greet.urls")),
+    path("", include("users.urls")),
 ]
