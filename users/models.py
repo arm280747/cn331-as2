cda2333e9018ac1cebeb0f62a98e2b0e02462abb
Natthasit Thitithammakun <6610685155@student.tu.ac.th>
2025-09-11 05:29:54 +0700

    Remove model Name

--- users/models.py
+++ users/models.py
@@ -1,9 +1,1 @@
 from django.db import models
-
-
-class Name(models.Model):
-    Name = models.CharField(max_length=100)
-    Surname = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.question_text
