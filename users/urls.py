b559d76d9251a8677bd72ad3056d1968276215cc
Athichart Penwong <arm280747@gmail.com>
2025-09-14 10:44:54 +0700

    fix cancel and room status

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.login_views, name="Login"),
-    path("admin/", admin.site.urls, name="Admin"),
     path("logout/", views.logout_views, name="Logout"),
     path("home/", views.index, name="home"),
     path("register/", views.register_views, name="Register"),
