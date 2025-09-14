9260835b46efe80f1850982801c963c35b023b2a
DemonicIV <nthitithammakun@gmail.com>
2025-09-14 07:07:53 +0700

    edit urls and views

--- Room/urls.py
+++ Room/urls.py
@@ -5,11 +5,13 @@ from . import views
 urlpatterns = [
     path("home/", views.home, name="home"),
     path("available-room/", views.allroom, name="available"),
-    #path("selectroom/<int:room_code>", views.room, name="selectroom"),
-    path("selectroom/", views.selectroom, name="selectroom"),
+    path("selectroom/<int:room_code>/", views.selectroom1, name="selectroom"),
     path("see-booking/", views.mybooking, name="mybooking"),
     path("addroom/", views.addroom, name="addroom"),
     path("saveroom/", views.saveroom, name="saveroom"),
     path("booking/", views.booking, name="booking"),
     path("cancel/", views.cancel, name="cancel"),
+    path("delete-room-page/", views.deletepage, name="deletepage"),
+    path("deleteroom", views.deleteroom, name="deleteroom"),
+    path("editroom/<int:room_code>/", views.editroom, name="editroom"),
 ]
--- Room/views.py
+++ Room/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse
 
-from .models import State, Room, Guest, Booking
+from .models import Room, Booking
 
 
 # Create your views here.
@@ -14,94 +14,81 @@ def allroom(request):
     all_room=Room.objects.all()
     return render(request, "room/available.html", {"allroom": all_room})
 
-
-def selectroom(request):
-    room_code=request.GET.get('id')
-    room = get_object_or_404(Room, pk=room_code)
-    return render(
-        request,
-        "room/selectroom.html",
-        {
-            "room": room,
-            "guest": room.guests.all(),
-            "non_guest": Guest.objects.exclude(rooms=room).all(),
-            # rooms มาจาก field ที่สร้างใน model Guest ส่วน guests มาจาก related name
-        },
-    )
-
-def selectroom1(request):
-    room_code=request.GET.get('id')
-    room = get_object_or_404(Room, pk=room_code)
+def selectroom1(request, room_code):
+    room = get_object_or_404(Room, room_code=room_code)
     context={
         "room_code":room_code,
         "room_name":room.room_name,
         "available_hours":room.available_hours,
-        "room_capacity":room.room_capacity
+        "room_capacity":room.room_capacity,
+        "is_available":room.is_available,
     }
     return render(request, "room/selectroom.html",context)
-
-# ใช้จองห้อง
-def booking_test(request, room_code):
-    if request.method == "POST":
-        room = get_object_or_404(Room, pk=room_code)
-        room_state = get_object_or_404(State, pk=room_code)
-        guest = get_object_or_404(Guest, pk=int(request.POST["guest"]))
-        # guest ใน method POST มาจาก name ของ input หรือ select ใน html
-
-        if guest not in room.guests.all() and room_state.room_available():
-            room_state.available_hours = room_state.available_hours - 1
-            room_state.save()
-
-            guest = Guest.objects.get(pk=int(request.POST["guest"]))
-            guest.rooms.add(room)  # หรือใช้ room.guests.add(guest) ก็ได้
-            guest.hours += 1
-            guest.save()
-
-            return render(
-                request,
-                reverse("mybooking", args=(room_state.code,)),
-                {"message": "booking success"},
-            )
-
-        elif guest in room.guests.all():
-            return render(
-                request,
-                reverse("room", args=(room_state.code,)),
-                {"message": "You're already book this room"},
-            )
-        else:
-            return render(
-                request,
-                reverse("room", args=(room_state.code,)),
-                {"message": "This room is full"},
-            )
-
-    return render(
-        request, reverse("home"), {"message": "some error occur please try again"}
-    )
 
 def addroom(request):
     room_list=Room.objects.all()
     return render(request, "room/addroom.html", {"roomlist": room_list})
+
 def saveroom(request):
     if request.method == "POST":
         room_code = request.POST["room_code"]
         room_name = request.POST["room_name"]
         room_capacity = request.POST["room_capacity"]
         available_hours = request.POST["available_hours"]
+        room_state = request.POST["room_state"]
         is_available = True
+
+        if room_state == "True":
+            is_available = True
+        else:
+            is_available = False
+
         room = Room.objects.create(
             room_code=int(room_code),
             room_name=room_name,
             room_capacity=room_capacity,
             available_hours=available_hours,
-            is_available=True
+            is_available=is_available
         )
         room.save()
         return redirect("addroom")
         #print(room.room_code)
 
     return render(request, "room/addroom.html")
+
+def deletepage(request):
+    room_list=Room.objects.all()
+    return render(request, "room/deleteroom.html", {"roomlist": room_list})
+
+def deleteroom(request):
+    if request.method == "POST":
+        room_code = request.POST["room_code"]
+        room = get_object_or_404(Room, room_code=room_code)
+        room.delete()
+        return redirect("deletepage")
+    return redirect("deletepage")
+
+def editroom(request, room_code):
+    room = get_object_or_404(Room, room_code=room_code)
+    if request.method == "POST":
+        check_exist = Room.objects.filter(room_code=request.POST["room_code"]).exists()
+        if check_exist:
+            return render(request, "room/editroom.html", {"room" : room, "message" : "This room code already exist."})
+        else:
+            room.room_code = request.POST["room_code"]
+            room.room_name = request.POST["room_name"]
+            room.room_capacity = request.POST["room_capacity"]
+            room.available_hours = request.POST["available_hours"]
+            room_state = request.POST["room_state"]
+
+            if room_state == "True":
+                room.is_available = True
+            else:
+                room.is_available = False
+            room.save()
+            return redirect("available")
+    return render(request, "room/editroom.html", {"room" : room })
+
 
 # ใช้ดูห้องที่จองไว้
 def mybooking(request):
@@ -112,14 +99,16 @@ def booking(request):
     if request.method == "POST":
         room_code = request.POST["room_code"]
         
-        check_booking = Booking.objects.filter(username = request.user.username,room_code=room_code)
+        #ใช้ room__room_code = room_code เพราะ Field room เป็น FK ไปยัง Room ทำให้ Booking ไม่มี Field room_code แต่อ้างแบบนี้ได้
+        check_booking = Booking.objects.filter(username = request.user.username,room__room_code=room_code)
         if not check_booking.exists():
+            room = Room.objects.get(room_code=int(room_code))
             booking = Booking.objects.create(
-                room_code=int(room_code),
+                room=room,
                 username=request.user.username
             )   
             #booking.save()
-            room = get_object_or_404(Room, pk=room_code)
+            room = get_object_or_404(Room, room_code=room_code)
             if room.available_hours>0:
                 room.available_hours -= 1
                 room.save()
